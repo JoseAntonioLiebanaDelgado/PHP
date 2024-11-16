2945e1cdde20f1f83fcd9b7388dcfb1d9301e004
Jose Antonio Liebana Delgado <jliebanadelgado@gmail.com>
2024-11-16 08:44:32 +0100

    Create exercise 1

---
+++ Exercises/S01-C01-Ejercicios/Ejercicio1/Ej1.php
@@ -0,0 +1,10 @@
+<?php
+
+/*Ejercicio 01 # Tarea 01: Información básica de PHP - I
+Muestra por pantalla la información básica del intérprete de PHP*/
+
+phpinfo();
+
+/*Guarda este código en un archivo PHP, por ejemplo, info.php.
+Cárgalo en tu servidor o ejecútalo localmente si tienes un servidor configurado en tu máquina (como XAMPP o WAMP).
+Abre el archivo en tu navegador (http://localhost/info.php) para ver la información del intérprete de PHP.*/
