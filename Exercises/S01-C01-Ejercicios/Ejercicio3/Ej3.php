9ea643567b13f8b7bed4a098eda4971987167df3
Jose Antonio Liebana Delgado <jliebanadelgado@gmail.com>
2024-11-24 13:21:24 +0100

    Create ex3

---
+++ Exercises/S01-C01-Ejercicios/Ejercicio3/Ej3.php
@@ -0,0 +1,24 @@
+<?php
+
+/*Ejercicio 02 # Tarea 01: Variables - I
+Crea variables de los siguientes tipos: numérica, texto, boleana, array. 
+Aplica la función “var_dump” y muestra el resultado por pantalla.*/
+
+
+$numerica = 10;
+$texto = "Hola Mundo";
+$boleana = true;
+$array = array(1, 2, 3, 4, 5);
+
+var_dump($numerica);
+var_dump($texto);
+var_dump($boleana);
+var_dump($array);
+
+
+/*La función var_dump() muestra información sobre una variable.
+var_dump() es particularmente útil para:
+
+-Depurar código y verificar el tipo de las variables.
+-Ver la estructura de arrays complejos u objetos.
+-Asegurarte de que las variables contienen lo que esperas cuando ejecutas tu código.*/
